2ed39d73f7790fa1d09fc9d2dc8c688dbd541fc8
alv67 <alv67.web@gmail.com>
2021-11-28 21:36:00 +0100

    bot/priority cloned from `bedtime`

---
+++ bots/priority/abn/jobs.py
@@ -0,0 +1,186 @@
+# Job
+# ---
+# The "Employment Office" for the Units 
+
+import math
+from typing import Tuple, List, Dict
+from lux.game_map import Position, Cell
+from lux.game_objects import Unit
+
+class Task:
+    """
+    Each task consist on a type of Job for a single unit.
+    The Task has to be done on a specified position, so the task is always 
+    movement and then the Job
+    """
+    SLEEP = "r"         # Sleep until daytime
+    BUILD = "b"         # Build a citytile of city 'city_id' at 'pos' 
+    HARVEST = "h"       # Get resouce at 'pos'
+    ENERGIZE = "e"      # Feed a 'city_id' with resource ad return it at 'pos'
+    EXPLORE = "x"       # Search for a place to build a new city
+
+def TaskStr(task: str) -> str:
+    _strings_ = {
+        Task.SLEEP: "SLP",
+        Task.BUILD: "BLD",
+        Task.HARVEST: "HRV",
+        Task.ENERGIZE: "NRG",
+        Task.EXPLORE: "XPL"
+    }
+
+    return(_strings_[task])
+
+
+class Job:
+    def __init__(self, task: str, pos: Position):
+        self.pos: Position = pos    # some task need a position reference
+        self.task: str = task       # task type
+        self.city_id: str = ""      # city_id parameter used by some tasks
+        self.unit_id: str = ""      # unit_id that has this task as assignement
+        self.subtask: int = 0       # subtask used by jobs with multistate tasks 
+        self.isNew : bool = True    # True if this job is assigned to a new unit
+
+    def __str__(self):
+        return f"{self.unit_id}: {TaskStr(self.task)}.{self.subtask} {self.pos} c:{self.city_id}"
+
+class JobBoard:
+    """
+    This is a Bulletin Board with 3 different lists:
+    - todo : list of jobs no-one has in charge
+    - inprogress : jobs currently in progress (with unit who has the assignement)
+    - done : completed jobs (only for debug pouposes) 
+    """
+
+    def __init__(self, parent):
+        self.todo: List[Job] = []        # ToDo Jobs
+        self.inprogress: Dict = {}  # InProgress Jobs
+        self.done: List = []        # Done Jobs
+        self.rip: List = []         # Dead Units
+        self.parent = parent
+
+    def _nextJob(self, unit: Unit):
+        # TODO: need a heuristics for job selection based on different parameters:
+        #       as unit position, time of day, priority.
+        #       Now no heurostics
+        if self.todo:
+            closest_dist = math.inf
+            i = 0
+            for n in range(len(self.todo)):
+                if self.todo[n].unit_id == unit.id: # not rejected job by this unit
+                    continue
+                dist = unit.pos.distance_to(self.todo[n].pos)
+                if dist < closest_dist:
+                    i = n
+                    dist = closest_dist            
+            return self.todo.pop(i)
+        else:
+            
+            # TODO: choose a default Job
+            return None
+
+    def _activeJobs(self):
+        """ Return a list with all active Jobs (not DONE) """
+        return self.todo + [n for n in self.inprogress.values()]
+
+    def addJob(self, task: str, pos: Position, city_id: str = "") -> bool:
+        # Check if no other active jobs are present for that position
+        if not pos in [ j.pos for j in self._activeJobs()]:
+            job = Job(task, pos)
+            job.city_id = city_id
+            self.todo.append(job)
+            return True
+        else:
+            return False
+    
+    def jobReject(self, unit_id: str):
+        """
+        The rejected job is returned from inProgress to ToDo list, 
+        mantains the unit_id so this job will not be assigned to the unit
+        that rejected it.
+        """
+        if unit_id in self.inprogress:
+            j = self.inprogress[unit_id]
+            j.subtask = 0
+            if j.task in [Task.EXPLORE, Task.BUILD]:
+                self.todo.append(j)
+            del self.inprogress[unit_id]
+
+    def count(self, task: str, pos: Position = None, city_id: str = "") -> int:
+        """
+        Return total number of active (not DONE) tasks with given parameters
+        """
+        retvalue = 0
+        # check in self.todo 
+        for job in self._activeJobs():
+            if job.task == task:                
+                if pos and pos != job.pos:
+                    continue
+                if city_id and city_id != job.city_id:
+                    continue
+                retvalue += 1
+        return retvalue 
+
+    def jobRequest(self, unit: Unit):
+        """ 
+        Unit can ask for a job, the function returns:
+        - an 'inprogress' job for that unit_id if it's present
+        - a new job from the 'todo' list
+        """
+        job = self.inprogress.get(unit.id)
+        if job:
+            job.isNew = False
+            return job
+        # check for the first Job from the 'todo' list
+        # if unit.get_cargo_space_left() > 0:
+        #    tile = self.parent.find_closest_resources(unit.pos)
+        #    if not tile:
+        #        job = Job(Task.SLEEP, unit.pos)
+        #    else: 
+        #        job = Job(Task.HARVEST, tile.pos)
+        #    job.unit_id = unit.id
+        #    self.inprogress[unit.id] = job
+        #    return job
+        #else:
+        job = self._nextJob(unit)
+        if job:            
+            job.unit_id = unit.id
+            job.isNew = True
+            job.subtask = 0
+            self.inprogress[unit.id] = job
+            return job
+        else: # none to do -> EXPLORE
+            job = Job(Task.EXPLORE, unit.pos)
+            job.isNew = True
+            job.unit_id = unit.id
+            self.inprogress[unit.id] = job
+            return job
+        
+    def jobDone(self, unit_id):
+        job = self.inprogress[unit_id]
+        self.done.append(job)
+        del self.inprogress[unit_id]
+
+    def jobDrop(self, unit_id):
+        job = self.inprogress[unit_id]
+        del self.inprogress[unit_id]
+
+
+    def activeJobToPos(self, pos: Position) -> bool:
+        return pos in [j.pos for j in self.inprogress.values()]
+
+    def checkActiveJobs(self, units : List):
+        """ 
+        Using list of Units check if there are some InProgress Jobs assigned to 
+        Unit no more on the list (dead unit). If the case then:
+        - return Job to ToDos
+        - add dead unit.id to rip List
+        """
+        morgue = []
+        for unit_id in self.inprogress:
+            if unit_id not in [u.id for u in units]:
+                morgue.append(unit_id)
+        for unit_id in morgue:
+            # mov job to self.todo
+            self.jobReject(unit_id)
+            # add unit_id in self.rip
+            self.rip.append(unit_id)
---
+++ bots/priority/agent.py
@@ -0,0 +1,317 @@
+import os
+import math
+import sys
+from typing import List, Tuple
+
+# for kaggle-environments
+from abn.game_ext import GameExtended
+from abn.jobs import Task, Job, JobBoard
+from abn.actions import Actions
+from lux.game_map import Position, Cell, RESOURCE_TYPES
+from lux.game_objects import City
+from lux.game_constants import GAME_CONSTANTS
+
+from lux import annotate
+
+MAX_CITY_SIZE = 5
+DISTANCE_BETWEEN_CITIES = 3
+        
+
+def find_closest_city_tile(pos, player):
+    closest_city_tile = None
+    if len(player.cities) > 0:
+        closest_dist = math.inf
+        # the cities are stored as a dictionary mapping city id to the city object, which has a citytiles field that
+        # contains the information of all citytiles in that city
+        for k, city in player.cities.items():
+            for city_tile in city.citytiles:
+                dist = city_tile.pos.distance_to(pos)
+                if dist < closest_dist:
+                    closest_dist = dist
+                    closest_city_tile = city_tile
+    return closest_city_tile
+
+
+def can_build_worker(player) -> bool:
+    # get nr of cytititles
+    nr_cts = 0
+    for k, c in player.cities.items():
+        nr_cts += len(c.citytiles)
+    return nr_cts > len(player.units)
+
+def city_can_expand(city: City, jobs: JobBoard) -> bool:
+    return len(city.citytiles) + jobs.count(Task.BUILD, city_id=city.cityid) < MAX_CITY_SIZE
+
+
+# Define global variables
+game_state = GameExtended()
+actions = Actions(game_state)
+lets_build_city = False
+build_pos = None
+jobs = game_state.job_board
+completed_cities = []
+
+def agent(observation, configuration, DEBUG=False):
+    global game_state
+    global actions
+    global lets_build_city
+    global build_pos
+    global completed_cities
+
+
+    ### Do not edit ###
+    game_state._update(observation)
+    actions.update()
+    path: List[Tuple] = []
+
+    ### AI Code goes down here! ###
+    player = game_state.player
+    opponent = game_state.opponent
+    # width, height = game_state.map.width, game_state.map.height
+    actions.append(annotate.sidetext(f"Time : {game_state.time}"))
+    actions.append(annotate.sidetext(f" {game_state.lux_time}h till night"))
+    if game_state.isMorning() : dbg = "Morning"
+    elif game_state.isEvening() : dbg = "Evening"
+    elif game_state.isNight() : dbg = "Night"
+    else: dbg = "Daytime"
+    actions.append(annotate.sidetext(f"it is {dbg}"))
+    
+    for _, city in player.cities.items():
+        # get energy cost for the night to come
+        cost = 10 * len(city.citytiles) * city.get_light_upkeep()
+        fulled = city.fuel > cost
+        city_size = len(city.citytiles)
+        # city can expand if actual city size + requested build is less than maximum size
+        # city_can_expand = city.cityid not in completed_cities
+        # Debug jobs.count
+        dbg = jobs.count(Task.BUILD, city_id=city.cityid)
+        actions.append(annotate.sidetext(f"{city.cityid}: {dbg} BLD"))
+        dbg = jobs.count(Task.ENERGIZE, city_id=city.cityid)
+        actions.append(annotate.sidetext(f"{city.cityid}: {dbg} NRG"))
+
+        # city_can_expand = city_size + jobs.count(Task.BUILD, city_id=city.cityid) < MAX_CITY_SIZE
+        if fulled:
+            if not city_can_expand(city, jobs):
+                pass
+                # completed_cities.append(city.cityid)
+                #jobs.addJob(Task.EXPLORE, Position(-1,-1), city_id= city.cityid)
+        #more_space = fulled and city_can_expand
+            else:
+                build_requested = False
+                pxy = Position(0,0)
+                for ct in city.citytiles:
+                    pxy = ct.pos
+                    # choose a place to create a new citytile in same city
+                    for x, y in [(pxy.x, pxy.y+1), (pxy.x, pxy.y-1), (pxy.x+1, pxy.y), (pxy.x-1, pxy.y)]:
+                        if not 0 <= x < game_state.map_width:
+                            continue
+                        if not 0 <= y < game_state.map_height:
+                            continue
+
+                        cell = game_state.map.get_cell(x, y)
+                        # actions.append(annotate.text(x, y, f"{x},{y}"))
+                        if cell.citytile:
+                            continue
+                        if cell.has_resource():
+                            continue
+                        if jobs.activeJobToPos(cell.pos):
+                            build_requested = True
+                            continue
+                        if city_can_expand(city, jobs):
+                            # actions.append(annotate.x(x, y))
+                            jobs.addJob(Task.BUILD, Position(x, y), city_id=city.cityid)
+                            build_requested = True
+                            break
+                    if build_requested:
+                        break
+                if not build_requested: # City can not expand
+                    completed_cities.append(city.cityid)
+                    jobs.addJob(Task.EXPLORE, Position(-1,-1), city_id= city.cityid)
+        for ct in city.citytiles:
+            pxy = ct.pos
+            actions.append(annotate.text(pxy.x, pxy.y, f"{fulled}"))
+            if ct.can_act():
+                if can_build_worker(player):
+                    actions.append(ct.build_worker())
+                else:
+                    actions.append(ct.research())
+            if not fulled and not game_state.isNight(): # and game_state.isEvening() :
+                if jobs.count(Task.ENERGIZE, city_id=city.cityid) < (city_size + 1) // 2:
+                    dbg = jobs.count(Task.ENERGIZE, city_id=city.cityid)
+                    dbg2 = (city_size + 1) // 2
+                    actions.append(annotate.sidetext(f"{city.cityid}: NRG {dbg} < {dbg2}"))
+                    jobs.addJob(Task.ENERGIZE, ct.pos, city_id = city.cityid)                                     
+
+    actions.append(annotate.sidetext(f"[INPROGRESS]"))        
+    for unit in player.units:
+        
+        # if the unit is a worker (can mine resources) and can perform an action this turn
+        if unit.is_worker():
+            my_job = jobs.jobRequest(unit)
+            actions.append(annotate.sidetext(f"{my_job}"))
+
+            if not unit.can_act():
+                continue
+
+            # Check if is evening time, if so, to survive, every
+            # job with risk of not having enough energy is dropped
+            # and a new HARVEST job is taken.
+            if game_state.isEvening():
+                if my_job.task == Task.BUILD or my_job.task == Task.EXPLORE:
+                    jobs.jobDrop(unit.id)
+                    my_job = jobs.jobRequest(unit)
+
+            if my_job.task == Task.HARVEST:
+                if unit.pos == my_job.pos:
+                    jobs.jobDone(unit.id)
+                else:
+                    move = unit.pos.path_to(my_job.pos, game_state.map, playerid=game_state.id)
+                    # move_dir = unit.pos.direction_to(my_job.pos)
+                    if not actions.move(unit, move.direction):
+                        jobs.jobReject(unit.id)
+
+            elif my_job.task == Task.ENERGIZE:
+                if my_job.subtask == 0: # search for resource
+                    if game_state.getEnergy(my_job.pos.x, my_job.pos.y) != 0:
+                        # citytile is adiacent to a resource so go directly there
+                        my_job.subtask = 1
+                    elif unit.get_cargo_space_left() == 0:
+                        my_job.subtask = 1
+                    elif (game_state.map.get_cell_by_pos(unit.pos).citytile or 
+                        game_state.getEnergy(unit.pos.x, unit.pos.y) == 0 ):
+                        tile = game_state.find_closest_resources(unit.pos)
+                        if not tile:
+                            jobs.jobReject(unit.id)
+                        else: 
+                            move = unit.pos.path_to(tile.pos, game_state.map, playerid=game_state.id)
+                            if not actions.move(unit, move.direction):
+                                jobs.jobReject(unit.id)
+                if my_job.subtask == 1: # go to citytile
+                    if unit.pos == my_job.pos:
+                        jobs.jobDone(unit.id)
+                    else:                
+                        move = unit.pos.path_to(my_job.pos, game_state.map, playerid=game_state.id)
+                        if not actions.move(unit, move.direction):
+                            jobs.jobReject(unit.id)
+                #if my_job.subtask == 2: # if citytile is adiacent to a resource stay here
+                #    if game_state.getEnergy(unit.pos.x, unit.pos.y) > 0:
+                #        if game_state.time >= 39:
+                #            jobs.jobDone(unit.id)
+                #    else:
+                #        jobs.jobDone(unit.id)
+
+            elif my_job.task == Task.BUILD:
+                if my_job.subtask == 0: # First need to full up unit
+                    if unit.get_cargo_space_left() == 0:
+                        my_job.subtask = 1
+                    elif (game_state.map.get_cell_by_pos(unit.pos).citytile or 
+                        game_state.getEnergy(unit.pos.x, unit.pos.y) == 0 ):
+                        tile = game_state.find_closest_resources(unit.pos)
+                        if not tile:
+                            jobs.jobDrop(unit.id)
+                        else: 
+                            move = unit.pos.path_to(tile.pos, game_state.map, playerid=game_state.id)
+                            if not actions.move(unit, move.direction):
+                                jobs.jobDrop(unit.id)
+                if my_job.subtask == 1: # Go to Build position
+                    if unit.pos == my_job.pos:
+                        if unit.get_cargo_space_left() > 0:
+                            jobs.jobDrop(unit.id)
+                        else:
+                            action = unit.build_city()
+                            actions.append(action)
+                            my_job.subtask = 2
+                    else:
+                        move = unit.pos.path_to(my_job.pos, game_state.map, noCities=True)
+                        if move.path:
+                            if not actions.move(unit, move.direction):
+                                jobs.jobReject(unit.id)
+                            # actions.append(unit.move(move_dir))
+                            # Draw the path
+                            actions.append(annotate.x(my_job.pos.x, my_job.pos.y))
+                            for i in range(len(move.path)-1):
+                                actions.append(annotate.line(
+                                    move.path[i][1], move.path[i][2], 
+                                    move.path[i+1][1], move.path[i+1][2]))                        
+                        else:   # not path found
+                            jobs.jobDone(unit.id)
+                if my_job.subtask == 2:
+                    # if city has adiacent energy then Unit Stay until new day
+                    if game_state.getEnergy(unit.pos.x, unit.pos.y) > 0:
+                        if game_state.time >= 39:
+                            jobs.jobDone(unit.id)
+                    else:
+                        jobs.jobDone(unit.id)
+
+            elif my_job.task == Task.SLEEP:
+                if unit.pos == my_job.pos:
+                    if game_state.time >= 39:
+                        jobs.jobDone(unit.id)
+                else:
+                    move_dir = unit.pos.direction_to(my_job.pos)
+                    if not actions.move(unit, move_dir):
+                        jobs.jobReject(unit.id)
+
+            elif my_job.task == Task.EXPLORE:
+                # this is a multistate task so my_job.subtask is the state
+                if my_job.subtask == 0: # find the position of resource (min 4 step from city)
+                    # get position of city that emitted the job
+                    if my_job.city_id in player.cities:
+                        pos = player.cities[my_job.city_id].citytiles[0].pos
+                    else:
+                        pos = my_job.pos
+                    res_cell = game_state.find_closest_resources(pos, min_distance=DISTANCE_BETWEEN_CITIES)
+                    if res_cell:
+                        my_job.subtask = 1  # HARVEST resource from position
+                        my_job.pos = res_cell.pos
+                    else:
+                        jobs.jobDone(unit.id)
+
+                if my_job.subtask == 1: # HARVEST resource from position
+                    if unit.pos == my_job.pos:
+                        if unit.get_cargo_space_left() > 0:
+                            if not game_state.map.get_cell_by_pos(unit.pos).has_resource:
+                                #jobs.jobReject(unit.id)
+                                jobs.jobDrop(unit.id)
+                        else: # next subtask
+                            my_job.pos = game_state.find_closest_freespace(unit.pos)
+                            my_job.subtask = 2  # BUILD A NEW CITY
+                    else:
+                        # move_dir = unit.pos.direction_to(my_job.pos)
+                        move = unit.pos.path_to(my_job.pos, game_state.map, playerid=game_state.id)
+                        if not actions.move(unit, move.direction):
+                            # jobs.jobReject(unit.id)
+                            jobs.jobDrop(unit.id)
+                if my_job.subtask == 2: # BUILD A NEW CITY
+                    if unit.pos == my_job.pos:
+                        # TODO: need to wait until next day
+                        action = unit.build_city()
+                        actions.append(action)
+                        my_job.subtask = 3 # WAIT UNTIL NEXT DAY
+                    else:
+                        #move_dir = unit.pos.direction_to(my_job.pos)
+                        move = unit.pos.path_to(my_job.pos, game_state.map, noCities=True, playerid=game_state.id)
+                        if not actions.move(unit, move.direction):
+                            # jobs.jobReject(unit.id) 
+                            jobs.jobDrop(unit.id)   
+                if my_job.subtask == 3: # WAIT UNTIL NEXT DAY
+                    if game_state.getEnergy(unit.pos.x, unit.pos.y) > 0:
+                        if game_state.time >= 39:
+                            jobs.jobDone(unit.id)
+                    else:
+                        jobs.jobDone(unit.id)
+
+    ## Debug Text
+    actions.append(annotate.sidetext(f"[TODO] {len(jobs.todo)}"))
+    for task in jobs.todo:
+        actions.append(annotate.sidetext(task))
+
+#    actions.append(annotate.sidetext(f"[INPROGRESS] {len(jobs.inprogress)}"))
+#    for task in jobs.inprogress:
+#        actions.append(annotate.sidetext(jobs.inprogress[task]))
+    
+#    actions.append(annotate.sidetext("-[CEMETERY]-"))
+#    for uid in jobs.rip:
+#        actions.append(annotate.sidetext(uid))
+
+    return actions.actions
